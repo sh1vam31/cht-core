6af2f87a56d8863370f88e1c194cd48b3ed30a56
ShivamChaudhary <sc31122004@gmail.com>
2026-03-23 12:35:55 +0530

    fix: add safety checks to prevent race condition in contact summary

--- config/default/contact-summary-extras.js
+++ config/default/contact-summary-extras.js
@@ -228,7 +228,7 @@ function getSubsequentPregnancies(allReports, refReport) {
 }
 
 function isActivePregnancy(thisContact, allReports, report) {
-  if (thisContact.type !== 'person' || !isAlive(thisContact) || !isPregnancyForm(report)) { return false; }
+  if (!thisContact || thisContact.type !== 'person' || !isAlive(thisContact) || !isPregnancyForm(report)) { return false; }
   const lmpDate = getMostRecentLMPDateForPregnancy(allReports, report) || report.reported_date;
   const isPregnancyRegisteredWithin9Months = lmpDate > today.clone().subtract(MAX_DAYS_IN_PREGNANCY, 'day');
   const isPregnancyTerminatedByDeliveryInLast6Weeks = getSubsequentDeliveries(allReports, report, 6 * 7).length > 0;
@@ -245,7 +245,7 @@ function isPregnant(allReports) {
 }
 
 function isReadyForNewPregnancy(thisContact, allReports) {
-  if (thisContact.type !== 'person') { return false; }
+  if (!thisContact || thisContact.type !== 'person') { return false; }
   const mostRecentPregnancyReport = getNewestReport(allReports, pregnancyForms);
   const mostRecentDeliveryReport = getNewestReport(allReports, deliveryForms);
   if (!mostRecentPregnancyReport && !mostRecentDeliveryReport) {
@@ -294,7 +294,7 @@ function isReadyForDelivery(thisContact, allReports) {
   //If pregnancy registration, date of LMP should be at least 6 months ago and no more than EDD + 6 weeks.
   //If pregnancy registration and no LMP, make it available at registration and until 280 days + 6 weeks from the date of registration.
   //If no pregnancy registration, previous delivery date should be at least 7 months ago.
-  if (thisContact.type !== 'person') {
+  if (!thisContact || thisContact.type !== 'person') {
     return false;
   }
   const latestPregnancy = getNewestReport(allReports, pregnancyForms);
