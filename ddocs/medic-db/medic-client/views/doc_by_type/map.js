c1a6e01ccb05644fe21652db12e827c08a0f1c9f
ShivamChaudhary <sc31122004@gmail.com>
2026-04-09 14:36:02 +0530

    fix: use named ES6 function for doc_by_type to satisfy SonarCloud and IDE

--- ddocs/medic-db/medic-client/views/doc_by_type/map.js
+++ ddocs/medic-db/medic-client/views/doc_by_type/map.js
@@ -1,6 +1,6 @@
 function map(doc) {
-  var typesToExclude = [ 'data_record', 'task', 'contact', 'target', 'telemetry' ];
-  if (doc.type && typesToExclude.indexOf(doc.type) === -1) {
+  const typesToExclude = [ 'data_record', 'task', 'contact', 'target', 'telemetry' ];
+  if (doc.type && !typesToExclude.includes(doc.type)) {
     emit([ doc.type ]);
   }
 }
