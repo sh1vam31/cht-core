2c1c0367b273499c0b5da66831b20f35064c895a
ShivamChaudhary <sc31122004@gmail.com>
2026-04-09 12:38:02 +0530

    feat: optimize doc_by_type view by excluding high-volume types

--- ddocs/medic-db/medic-client/views/doc_by_type/map.js
+++ ddocs/medic-db/medic-client/views/doc_by_type/map.js
@@ -1,3 +1,6 @@
-function(doc) {
-  emit([ doc.type ]);
+function map(doc) {
+  var typesToExclude = [ 'data_record', 'task', 'contact', 'target', 'telemetry' ];
+  if (doc.type && typesToExclude.indexOf(doc.type) === -1) {
+    emit([ doc.type ]);
+  }
 }
